9cb0d80d95794760e9466981e4c2f64a3cc3622a
pirat <popov1vadym@gmail.com>
2017-11-15 01:14:04 +0300

    the game where you have to type the word and the next one should start on the last letter of previous word

---
+++ GameTowns/GameTowns/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTowns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city1 = "";
+            string city2 = "";
+            city1 = Console.ReadLine();
+            city2 = Console.ReadLine();
+
+            string[] arrOfCities = new string[2];
+            arrOfCities[0] = city1;
+            arrOfCities[1] = city2;
+
+            do
+            {
+
+                if (city1[city1.Length - 1] == city2[0])
+                {
+                    city1 = city2;
+                    city2 = Console.ReadLine();
+                }
+                else
+                {
+                    do
+                    {
+                        Console.WriteLine("false.Try Again");
+                        city2 = Console.ReadLine();
+                    }
+                    while (city2[0] != city1[city1.Length - 1]);
+                }
+                Array.Resize(ref arrOfCities, arrOfCities.Length + 1);
+                arrOfCities[arrOfCities.Length - 1] = city2;
+
+            } while (city1 != "stop" && city2 != "stop");
+
+            Console.WriteLine("=============Finall============");
+
+            for (int i = 0; i < arrOfCities.Length; i++)
+            {
+                Console.WriteLine("The cities which were used");
+                Console.WriteLine(arrOfCities[i]);
+            }
+        }
+    }
+}
